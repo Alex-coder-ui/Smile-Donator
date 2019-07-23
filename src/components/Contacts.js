52b15235942baa6afa6e8917003935dbe6f95137
Alex <jopavsem89@gmail.com>
2019-07-24 02:08:14 +0300

    added routes

---
+++ src/components/Contacts.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Contacts extends Component {
+
+    render(){
+        return <h2>Contacts</h2>;
+    }
+
+}
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,33 @@
 
-import React from "react"
+import React, {Component} from 'react';
 import { Navbar, Nav, NavItem, NavDropdown,MenuItem } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Contacts from './Contacts.js';
+import Donate from './Donate.js';
+import Calendar from './Calendar.js';
 
-export class Header extends React.Component {
+
+function Index() {
+    return <h2>Home</h2>;
+}
+
+
+
+export class Header extends Component {
     render() {
         return (
+            <Router>
             <div>
                 <Navbar fixed="top" />
                 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Simle Donator</Navbar.Brand>
+                    <Navbar.Brand href="/">Simle Donator</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#Contacts">Contacts</Nav.Link>
-                            <Nav.Link href="#Donate">Donate</Nav.Link>
+                            <Nav.Link href="Contacts">Contacts</Nav.Link>
+                            <Nav.Link href="Donate">Donate</Nav.Link>
                             <NavDropdown title="Click me!" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#View calendar">View calendar</NavDropdown.Item>
+                                <NavDropdown.Item href="Calendar">View calendar</NavDropdown.Item>
                                 <NavDropdown.Item href="#Registration form">Registration form</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="https://www.google.com/search?q=kitties+images&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjSq9rQ-MvjAhWq_CoKHWRPDSYQ_AUIESgB&biw=1600&bih=789#imgrc=9e9KbvJx7Q6h_M:">View kitties images</NavDropdown.Item>
@@ -23,7 +35,12 @@ export class Header extends React.Component {
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
+                <Route path="/" exact component={Index} />
+                <Route path="/Contacts/" component={Contacts} />
+                <Route path="/Donate/" component={Donate} />
+                <Route path="Calendar" exact component={Calendar} />
             </div>
+            </Router>
         );
     }
 }
