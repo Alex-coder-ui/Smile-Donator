317bc6011702265af089c49ce76130be73c5a379
Alex <jopavsem89@gmail.com>
2019-07-31 03:37:07 +0300

    added simple validation in cintacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/es/FormControl";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 
 
 export default class Contacts extends Component {
@@ -9,84 +10,109 @@ export default class Contacts extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: "",
             name: "",
             email: "",
-            message: ""
+            message: "",
         };
 
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.submitButtonClick = this.submitButtonClick.bind(this)
 
     }
-
-
-    handleNameChange(event) {
-        console.log('handle name change');
-
-        this.setState({
-            name: event.target.value
-        });
-
-        console.log("New state " + this.state.name);
-    }
-
-
-    submitButtonClick(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-        this.setState({
-            [name]: value
-        })
-        console.log('A name was submitted: ' + this.state.value)
-    }
-
 
     render() {
+        return <div className="Input-group">
+            <UserData/>
+        </div>
 
-        const name = this.state.name;
-        return (
-            <div className="Input-group">
+        function UserData() {
 
-                <InputGroup className="mb-3" style={{width: "60%",margin: "15px", padding: "auto"}}>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="inputGroup-name">Name</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
+            const [validated, setValidated] = React.useState(false);
+            const handleSubmit = event => {
+                const form = event.currentTarget;
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
 
-                        onChange={this.handleNameChange}
-                        value={name}
-                        aria-label="Name"
-                        aria-describedby="inputGroup-name"
-                    />
-                </InputGroup>
-                <br/>
+                setValidated(true);
+            }
 
-                <InputGroup className="mb-3" style={{width: "60%",margin: "15px", padding: "auto"}}>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="inputGroup-email">Email</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        aria-label="Email"
-                        aria-describedby="inputGroup-email"
-                    />
-                </InputGroup>
-                <br/>
+            return (
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Row>
+                        <Form.Group md="4" controlId="validationCustom01"
+                                    style={{width: "60%", margin: "15px", padding: "auto"}}>
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="First name"
+                                defaultValue="Alex"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group md="4" controlId="validationCustom02"
+                                    style={{width: "60%", margin: "15px", padding: "auto"}}>
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Last name"
+                                defaultValue="Coder"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group md="4" controlId="formGridEmail"
+                                    style={{width: "60%", margin: "15px", padding: "auto"}}>
+                            <Form.Label>Email</Form.Label>
+                            <InputGroup>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Email"
+                                    aria-describedby="inputGroupPrepend"
+                                    required
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter your email.
+                                </Form.Control.Feedback>
+                            </InputGroup>
+                        </Form.Group>
+                    </Form.Row>
 
 
-                <InputGroup style={{width: "60%", margin: "15px", padding: "auto"}}>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="inputGroup-comment">Comment</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl as="textarea" aria-label="Comment" aria-describedby="inputGroup-comment"/>
-                </InputGroup>
 
-                <Button variant="primary" onClick={this.submitButtonClick} style={{ padding: "15px",  margin: "15px"}}>Save changes</Button>
-            </div>
+                    <Form.Group controlId="exampleForm.ControlTextarea1"
+                                style={{width: "60%", margin: "15px", padding: "auto"}}>
+                        <Form.Label>Comment</Form.Label>
+                        <Form.Control as="textarea"
+                                      rows="3"/>
+                    </Form.Group>
 
+                    <Form.Group>
+                        <Form.Check
+                            required
+                            label="Agree to send my data to the test server"
+                            feedback="You must agree before submitting."
+                        />
+                    </Form.Group>
 
-        );
+                    <Button type="submit" style={{padding: "15px", margin: "15px"}}>Send data</Button>
+                </Form>
+
+            )
+
+        }
 
     }
 }
+
+
+
